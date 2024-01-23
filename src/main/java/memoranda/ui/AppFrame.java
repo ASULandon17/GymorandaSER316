85c9ebf3470c8da7bde68b2237ec1e61afbce346
Jake <jwsmit24@asu.edu>
2024-01-23 15:11:38 -0600

    Located bug to prevent preferences window from opening - in progress

--- src/main/java/memoranda/ui/AppFrame.java
+++ src/main/java/memoranda/ui/AppFrame.java
@@ -835,6 +835,7 @@ public class AppFrame extends JFrame {
 
     public void showPreferences() {
         PreferencesDialog dlg = new PreferencesDialog(this);
+        // issues are caused when pack is called
         dlg.pack();
         dlg.setLocationRelativeTo(this);
         dlg.setVisible(true);
