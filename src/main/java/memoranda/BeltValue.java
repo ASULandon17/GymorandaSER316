f9abeb0cdc45219e289b6533f1684cefc3472550
cthieme1 <cthieme1@asu.edu>
2024-02-01 03:28:06 -0800

    US-#42 -- Task#72 Create enum type BeltValue for belt data

---
+++ src/main/java/memoranda/BeltValue.java
@@ -0,0 +1,51 @@
+package main.java.memoranda;
+
+/**
+ * Enum class for the belt ranks
+ */
+public enum BeltValue {
+    WHITE, YELLOW, ORANGE, PURPLE, BLUE, BLUE_STRIPE, GREEN, GREEN_STRIPE,
+    BROWN1, BROWN2, BROWN3, BLACK1, BLACK2, BLACK3;
+
+    /**
+     * Increases the belt rank by one stage
+     * Does not do anything if there are no more stages
+     * @return new belt rank
+     */
+    public BeltValue increaseBelt() {
+        switch(this) {
+            case WHITE:
+                return YELLOW;
+            case YELLOW:
+                return ORANGE;
+            case ORANGE:
+                return PURPLE;
+            case PURPLE:
+                return BLUE;
+            case BLUE:
+                return BLUE_STRIPE;
+            case BLUE_STRIPE:
+                return GREEN;
+            case GREEN:
+                return GREEN_STRIPE;
+            case GREEN_STRIPE:
+                return BROWN1;
+            case BROWN1:
+                return BROWN2;
+            case BROWN2:
+                return BROWN3;
+            case BROWN3:
+                return BLACK1;
+            case BLACK1:
+                return BLACK2;
+            case BLACK2:
+                return BLACK3;
+            case BLACK3:
+                System.out.println("Cannot increase rank any further.");
+                return this;
+            default:
+                System.out.println("Error increasing belt rank.");
+                return this;
+        }
+    }
+}
