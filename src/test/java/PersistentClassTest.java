6eaa03cf5b71532110204b889457dfcd27e1fbfe
Jake <jwsmit24@asu.edu>
2024-02-02 15:30:03 -0600

    US-#40/77 add setup conditions for test cases

--- src/test/java/PersistentClassTest.java
+++ src/test/java/PersistentClassTest.java
@@ -1,22 +1,48 @@
 import memoranda.PersistentClass;
-import main.java.memoranda.User;
+import memoranda.User;
 import main.java.memoranda.UserType;
 
 import org.junit.*;
+
+import java.io.File;
+
+
 import static org.junit.Assert.*;
 
 public class PersistentClassTest {
 
 
+    public void deleteJSONFiles() {
+        try{
+            File doomedClasses = new File("classes.json");
+            File doomedUsers = new File("users.json");
+
+           if(doomedClasses.delete() && doomedUsers.delete()) {
+               System.out.println("JSON files deleted succesfully for testing");
+           } else{
+               System.out.println("JSON files were not deleted");
+           }
+
+
+        }catch (SecurityException | NullPointerException e) {
+            System.out.println("Program does not have access to delete this file");
+        }
+
+
+
+
+
+    }
     @Before
     public void setup() {
+        deleteJSONFiles();
         User.signUp("trainerTest", "password", UserType.TRAINER);
     }
 
     @Test
     public void testAddNewClassWithoutTrainer () {
         assertTrue(PersistentClass.addNewClass("spin class", 2, 10, 1));
-        assertTrue(PersistentClass.addNewClass("swim class", 3, 10, 2));
+
 
 
     }
