e98be462139c24a3968cd6be242071ed53a1ea47
Natel Whitaker <nawhitak@asu.edu>
2024-02-10 20:51:17 +0700

    fixed logic erorr with getTrainer and added comments

--- src/main/java/memoranda/TrainerList.java
+++ src/main/java/memoranda/TrainerList.java
@@ -5,13 +5,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Vector;
+
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+/**
+ * TrainerList
+ * contains methods that get either a trainer or all trainers out of ones that are signed up in the system
+ */
 public class TrainerList {
+    /**
+     * TrainerList
+     * constructor for creating a new TrainerList
+     */
+    public TrainerList() {
+    }
 
-    public TrainerList() {}
-
+    /**
+     * getTrainers
+     * searches for currently signed up trainers and adds them all to a list to be returned.
+     *
+     * @return a vector filled with the currently signed up trainers
+     */
     public Vector<Trainer> getTrainers() {
         Vector<Trainer> trainers = new Vector<>();
         try {
@@ -36,6 +52,13 @@ public class TrainerList {
         return trainers;
     }
 
+    /**
+     * getTrainer
+     * searches for a trainer given their name
+     *
+     * @param name the name of the specified trainer
+     * @return returns a trainer's information if found
+     */
     public Trainer getTrainer(String name) {
         Trainer trainer = null;
         try {
@@ -48,7 +71,7 @@ public class TrainerList {
             JSONArray usersArray = new JSONArray(content);
             for (int i = 0; i < usersArray.length(); i++) {
                 JSONObject user = usersArray.getJSONObject(i);
-                if (user.getString("username").equals(name)) {
+                if (user.getString("username").equals(name) && user.getString("userType").equals("TRAINER")) {
                     trainer = new Trainer(user.getString("username"), getRank(user.getString("beltRank")), getRank(user.getString("trainingRank")));
                 }
             }
@@ -60,9 +83,16 @@ public class TrainerList {
         return trainer;
     }
 
+    /**
+     * getRank
+     * converts the given string into the equivalent belt value
+     *
+     * @param userRank string value of user's rank
+     * @return belt value of user's rank
+     */
     private BeltValue getRank(String userRank) {
         BeltValue rank = null;
-        switch(userRank) {
+        switch (userRank) {
             case "NO_BELT":
                 rank = BeltValue.NO_BELT;
                 break;
