58a2561bb675712903b6d571826860ff60ed2ae1
Jake <jwsmit24@asu.edu>
2024-02-16 06:30:59 -0600

    Storyless - Add javadoc to LookAndFeel

--- src/main/java/memoranda/ui/gymoranda/LookAndFeel.java
+++ src/main/java/memoranda/ui/gymoranda/LookAndFeel.java
@@ -3,14 +3,21 @@ package main.java.memoranda.ui.gymoranda;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class contains UI helper methods for consistent design across Gymoranda.
+ */
 public class LookAndFeel {
 
-
+    /**
+     * Look and feel for Gymoranda buttons.
+     * @param button - button to configure
+     */
     public static void gymButtonHelper(JButton button) {
 
         button.setFocusable(false);
         button.setBorderPainted(false);
         button.setContentAreaFilled(false);
+
         button.setOpaque(true);
         button.setBackground(new Color(0, 100, 0));
         button.setForeground(Color.WHITE);
@@ -19,6 +26,9 @@ public class LookAndFeel {
     }
 
 
+    /**
+     * Private constructor for util class.
+     */
     private LookAndFeel() {
         // No objects here!
     }
